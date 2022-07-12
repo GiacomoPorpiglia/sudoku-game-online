7bf9a0e43ee9efd87a59e406e5ca56400be1e62a
GiacomoPorpiglia <giacomo.porpiglia@gmail.com>
2022-07-12 12:56:15 +0200

    Style adjustments

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,15 @@ import Home from './components/Home'
 import Navbar from './components/Navbar'
 
 const App = () => {
+
+
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  window.addEventListener('resize',  () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`); 
+  })
   
   return (
     <>
